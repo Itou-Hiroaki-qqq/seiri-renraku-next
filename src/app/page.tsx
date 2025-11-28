938c6ecec8d31aa5e916f73d2ae6ae77bcef53e7
Itou-Hiroaki-qqq <chiteijin315@gmail.com>
2025-11-29 04:08:07 +0900

    vercelトップページ修正：src/app/page.tscに統合

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <p>トップページです。リンクはこちら → /home</p>;
+'use client';
+
+import InputForm from '../components/InputForm';
+import CardList from '../components/CardList';
+import ClientAppWrapper from '../components/ClientAppWrapper';
+
+export default function Home() {
+    return (
+        <>
+            <ClientAppWrapper />
+            <InputForm />
+            <CardList />
+        </>
+    );
 }
