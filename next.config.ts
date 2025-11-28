d7101018b3719a1f2cb796ff3702adf532e61468
Itou-Hiroaki-qqq <chiteijin315@gmail.com>
2025-11-29 03:59:37 +0900

    vercel/Not Found対応：next.config修正(2)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  // 本番だけ basePath を追加
-  basePath: isProd ? "/app/seirishite-renrakukun" : "",
 };
 
 export default nextConfig;
