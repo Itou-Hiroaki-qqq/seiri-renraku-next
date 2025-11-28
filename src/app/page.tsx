5f4c76aaaf5dfe76a858ac7477aa4b17c9ca588a
Itou-Hiroaki-qqq <chiteijin315@gmail.com>
2025-11-28 13:43:56 +0900

    vercelデプロイエラー対応：fix: disable SSR for HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-import HomePage from "../pages/HomePage";
+"use client";
+
+import dynamic from "next/dynamic";
+
+// HomePage を動的読み込み (SSR を無効化)
+const HomePage = dynamic(() => import("../pages/HomePage"), {
+  ssr: false, // プリレンダリングを防ぐ
+});
 
 export default function Page() {
   return <HomePage />;
